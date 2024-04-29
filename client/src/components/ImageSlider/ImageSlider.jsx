d12694fc0e94f405c877f22edc2c4cdc8c7d69af
Savelev Andrey <needway@list.ru>
2024-04-29 17:45:41 +0300

    yandex maps added
    +YaMaps in About section

---
+++ client/src/components/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ImageSlider = () => {
+  return (
+    <div>ImageSlider</div>
+  )
+}
--- client/src/pages/Blog/Blog.jsx
+++ client/src/pages/Blog/Blog.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import styles from './Blog.module.css';
 
 export const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.container}>
+      <div className={styles.imageSlider}>
+        {/*<ImageSlider imageUrls={IMAGES} />
+        */}
+      </div>
+      </div>
   )
 }
